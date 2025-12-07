eab7c2f01e181d37ba572a35cdb2838b573067c8
yunfeil <yunfeil@kth.se>
2025-12-07 18:24:23 +0100

    small changes

--- src/time4riscv/Entities.h
+++ src/time4riscv/Entities.h
@@ -1,11 +1,15 @@
 #ifndef ENTITIES_H  // 1. Check if the guard symbol is NOT defined
 #define ENTITIES_H  // 2. Define the guard symbol
 
+#define Food_count 50
+#define MIN_RADIUS 10
 typedef struct {
     int x_pos;
     int y_pos;
     int radius;
     int hp;
+    int dx;
+    int dy;
     int velocity;
     int color;
 } Player;
--- src/time4riscv/GameState.h
+++ src/time4riscv/GameState.h
@@ -1,5 +1,5 @@
-#ifndef GAMESTATE_H  // 1. Check if the guard symbol is NOT defined
-#define ENTITIES_H 
+#ifndef GAMESTATE_H
+#define GAMESTATE_H
 
 #include "Entities.h"
 
@@ -7,22 +7,7 @@ typedef struct {
     int var1;
     int var2;
     Player players[2]; // Can hold two players
-    Food crumbs[20]; // Can hold five food pieces
+    Food crumbs[Food_count]; // Can hold five food pieces
 } GameState;
 
-void GameState_generate_playes(GameState* gms, int num_p);
-
-void GameState_init(){
-    // Initialize game state
-}
-
 #endif
-
-
-
-
-
-
-
-
-
