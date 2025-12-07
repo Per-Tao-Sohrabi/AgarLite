c8b18547ab7cd054521ed5575b2949b45f73a874
Per Sohrabi <psohrabi@kth.se>
2025-12-08 00:41:09 +0100

    Updated prog_states from testing2 to allow for render

--- src/time4riscv/prog_states.c
+++ src/time4riscv/prog_states.c
@@ -2,6 +2,12 @@
 #include "prog_states.h"
 #include "GameState.h"
 #include "inputs.h"
+#include "render.h"
+
+extern void clear_screen();
+extern void draw_string_wrapped(int x, int y, const char *str, int color, int max_width);
+
+
 
 // Initalize a game state
 GameState run_start_up() {
@@ -17,22 +23,26 @@ GameState run_start_up() {
 
 /* Query the player about the game mode they want to play at*/
 int query_game_mode() {
+    clear_screen();   
     char* msg = "Select Game Mode: 1 or 2 Players by toggling the first switch up for single player. Switch up down for multiplayer. Press button to confirm.\n";
-
+    draw_string_wrapped(80, 60, &msg, WHITE, MSG_WIDTH);
     print(msg); // render function
     // Wait for user input and return selected mode
     while(1) {
         int mode = get_switch_state(0);      // Read the mode input
         if(get_btn() == 1) {                 // Poll the button
+            clear_screen();
             return mode;                     // Return set mode
         }
     }
+    clear_screen();
     return 1; // Placeholder return value
 }
 
 /* Query the player about the difficulty they want to play at*/
 int query_game_difficulty() {
     char* msg = "Use the three first switches to set your difficulty. Note binary numbers! Press button to confirm.\n";
+    draw_string_wrapped(80, 60, &msg, WHITE, MSG_WIDTH);
     print(msg); // render function
     
     // Wait for user input and return selected mode
@@ -74,8 +84,10 @@ int query_game_difficulty() {
             default:
                 break;
             }
+            clear_screen();
         }
     }
+    clear_screen();
     return 1; // Placeholder return value
 }
  
@@ -84,7 +96,12 @@ void run_pause() {
     int status = get_switch_state(4);
     while(1) {
         print("Toggle switch 4 down to exit pause");
+        char* msg = "Toggle switch 4 down to exit pause";
+        clear_screen();
+        draw_string_wrapped(80, 60, &msg, WHITE, MSG_WIDTH);
+
         if(status = 0) {
+            clear_screen();
             break;
         }
     }
@@ -93,6 +110,9 @@ void run_pause() {
 /* Game Over*/
 void run_game_over() {
     print("Game Over! Press button to restart.");
+    clear_screen();
+    char* msg = "Game Over! Press button to restart.";
+    draw_string_wrapped(80, 60, &msg, WHITE, MSG_WIDTH);
     while(1) {
         if(get_btn() == 1) {
             break;
