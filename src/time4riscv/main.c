a080dd75ed59ee3f94dd8a97c5ec7d41f6d86cab
Per Sohrabi <psohrabi@kth.se>
2026-03-01 15:15:47 +0100

    feat: Enable Interrupt on Timer Overflow (ITO) by updating the timer[0] configuration.

--- src/time4riscv/main.c
+++ src/time4riscv/main.c
@@ -47,7 +47,7 @@ void labinit(void) {
   timer[3] = (period_val >> 16) & 0xFFFF;
 
   // Set start statuss
-  timer[0] = 0b1110; // Enable timer, sets ito = off, cont = on, start = on, stop. = off. 
+  timer[0] = 0b0111; // ITO = on, CONT = on, START = on, STOP = off
   print("---- Timer start status set.\n");
 }
 
